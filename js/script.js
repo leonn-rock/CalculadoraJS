2a891d1d3facfd412e16818de11d4c1e41a82cdf
leonn-rock <139873102+leonn-rock@users.noreply.github.com>
2023-08-16 12:04:49 -0300

    Função cientifica
    Botao mod e funcoes cientificas

--- js/script.js
+++ js/script.js
@@ -6,7 +6,26 @@ var operador = ""
 var result = 0
 var cliquePonto = false
 var tema = false
+var limitador = 8
+var rad = false
 
+function modo(){
+    var divCalculadora = document.querySelector("#calc")
+    var divCientifica = document.querySelector("#cient")
+    
+    if (divCalculadora.classList.contains("calculadora") && divCientifica.classList.contains("cientificaOFF")){
+        divCalculadora.classList.remove("calculadora")
+        divCientifica.classList.remove("cientificaOFF")
+        divCalculadora.classList.add("cientifica")
+        divCientifica.classList.add("cientificaON")
+    } else {
+        divCalculadora.classList.remove("cientifica")
+        divCientifica.classList.remove("cientificaON")
+        divCalculadora.classList.add("calculadora")
+        divCientifica.classList.add("cientificaOFF")
+    }
+    trocaTipo()
+}
 function verifica(){
     if (identificador==true){
         y = A
@@ -19,6 +38,22 @@ function exibe(fator){
     verifica()
     document.querySelector("#tela").textContent = parseFloat(fator.join(""))
 }
+function trocaTipo(){
+    var tipo = document.querySelector("#tipo")
+    if(rad == true){
+        rad = false
+        tipo.textContent = "DEG"
+    }else{
+        rad = true
+        tipo.textContent = "RAD"
+    }
+}
+function tipo(){
+    if (rad == false){
+        num1 = num1 * Math.PI
+        num2 = num2 * Math.PI
+    }
+}
 function estilo(){
     if(tema == false){
         document.body.style.backgroundColor = "#121212"
@@ -30,7 +65,10 @@ function estilo(){
 }
 function addNum(num){
     verifica()
-    y.push(num)
+    console.log(y.length < 8)
+    if (y.length <8 ){
+         y.push(num)
+    }
     exibe(y)
 }
 function ponto(){
@@ -45,7 +83,7 @@ function operacao(aux){
     identificador = false
     cliquePonto = false
     operador = aux.toString()
-    if( aux == 5 || aux == 6){
+    if( aux == 5 || aux == 6 || aux == 8 || aux == 10 || aux == 11 || aux == 12 || aux == 13){
         igual()
     }
 }
@@ -71,34 +109,52 @@ function igual(){
     switch (operador) {
         case '1':
             result = num1 + num2
-            break;
+            break
         case '2':
             result = num1 - num2
-            break; 
+            break
         case '3':
             result = num1 * num2
-            break; 
+            break 
         case '4':
             if( num2 == 0){
                 result = "Indeterminação" 
             }else{
                 result = num1 / num2
             }
-            break;  
+            break 
         case '5':
             result = Math.sqrt(num1) 
-            break;  
+            break 
         case '6':
             result = num1**2
-            break;  
+            break 
         case '7':
             result = (num1 * num2) / 100
-            break;  
+            break 
+        case '8':
+            result = Math.log(num1)
+            break
+        case '9':
+            result = num1**num2
+            break
+        case '10':
+            result = Math.tan(num1)
+            break
+        case '11':
+            result = Math.cos(num1)
+            break
+        case '12':
+            result = Math.sin(num1)
+            break
+
+            
     }          
-    document.querySelector("#tela").textContent = result
+    document.querySelector("#tela").textContent = parseFloat(result.toFixed(limitador))
     identificador = true
     A = []
     B = []
+    cliquePonto = false
 }
 function cleanElement(){
     verifica()
